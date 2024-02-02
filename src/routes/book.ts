e3bed81d4f453eecb66e5acb3019cfeb56fd358b
zahinafsar <afsarzahin@gmail.com>
2024-02-02 18:20:27 +0600

    feat: add get all books api

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -38,7 +38,7 @@ router.put("/:id", async (req, res) => {
       price,
     },
   });
-  
+
   if (!book) {
     res.status(404).json({ message: `book with id: ${id} was not found` });
   } else res.status(200).json(book);
@@ -56,9 +56,40 @@ router.get("/:id", async (req, res) => {
   else res.status(200).json(book);
 });
 
-router.get("/", async (req, res) => {
-  const books = await prisma.books.findMany();
-  res.status(200).json(books);
-});
+router.get('/', async (req, res) => {
+
+  const {
+    title,
+    author,
+    genre,
+    sort,
+    order,
+  } = req.query
+
+  const books = await prisma.books.findMany({
+    where: {
+      OR: [
+        {
+          title: {
+            contains: title as string
+          }
+        }, {
+          author: {
+            contains: author as string
+          }
+        }, {
+          genre: {
+            contains: genre as string
+          }
+        }
+      ]
+    },
+    orderBy: {
+      ...(sort ? { [sort as string]: order || 'asc' } : {})
+    }
+  });
+
+  res.status(200).json(books)
+})
 
 export const bookRouter = router;
