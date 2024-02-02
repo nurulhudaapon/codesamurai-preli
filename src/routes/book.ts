6ab5ef91b53baede3504c75c1761d704e0e9bc76
Nurul Huda (Apon) <me@nurulhudaapon.com>
2024-02-02 17:52:38 +0600

    feat: add testing and fix errors

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,58 +1,64 @@
 // express routes for book
-import express from "express"
-import { prisma } from "../db"
+import express from "express";
+import { prisma } from "../db";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/', async (req, res) => {
-  const { id, title, author, genre, price } = req.body
-  const book = await prisma.books.create({
-    data: {
-      id,
-      title,
-      author,
-      genre,
-      price
-    }
-  })
-  res.status(201).json(book);
-})
+router.post("/", async (req, res) => {
+  const { id, title, author, genre, price } = req.body;
 
-router.put('/:id', async (req, res) => {
-  const { id } = req.params
-  const { title, author, genre, price } = req.body
+  try {
+    const book = await prisma.books.create({
+      data: {
+        id,
+        title,
+        author,
+        genre,
+        price,
+      },
+    });
+
+    res.status(201).json(book);
+  } catch (error) {
+    res.status(400).json({ message: "error creating book" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, author, genre, price } = req.body;
   const book = await prisma.books.update({
     where: {
-      id: parseInt(id)
+      id: parseInt(id),
     },
     data: {
       title,
       author,
       genre,
-      price
-    }
-  })
-  if(!book) {
-     res.status(404).json({"message": `book with id: ${id} was not found`})
-  }
-  else res.status(200).json(book);
-})
+      price,
+    },
+  });
+  
+  if (!book) {
+    res.status(404).json({ message: `book with id: ${id} was not found` });
+  } else res.status(200).json(book);
+});
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
   const book = await prisma.books.findUnique({
     where: {
-      id: parseInt(id)
-    }
-  })
-  if(!book)  res.status(404).json({"message": `book with id: ${id} was not found`})
-  res.status(200).json(book)
-})
+      id: parseInt(id),
+    },
+  });
+  if (!book)
+    res.status(404).json({ message: `book with id: ${id} was not found` });
+  else res.status(200).json(book);
+});
 
-
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const books = await prisma.books.findMany();
-  res.status(200).json(books)
-})
+  res.status(200).json(books);
+});
 
 export const bookRouter = router;
