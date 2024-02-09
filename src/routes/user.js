9b98eecf21a7c1f63fa2403b71597b039ebca358
Nurul Huda (Apon) <me@nurulhudaapon.com>
2024-02-09 13:02:59 +0000

    feat: wallet route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,28 +1,19 @@
 import express from "express";
-import { prisma } from '../db.js'
+import { prisma } from "../db.js";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const {
-    user_id,
-    user_name,
-    balance
-  } = req.body;
+  const { user_id, user_name, balance } = req.body;
 
-  try {
-    const newUser = await prisma.users.create({
-      data: {
-        user_id,
-        user_name,
-        balance
-      }
-    });
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ message: "" });
-  }
+  const newUser = await prisma.users.create({
+    data: {
+      user_id,
+      user_name,
+      balance,
+    },
+  });
+  res.status(201).json(newUser);
 });
 
 export { router as userRouter };
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import { userRouter } from "./routes/user.js"
 import { stationRouter } from "./routes/station.js";
 import { trainRouter } from "./routes/train.js";
+import { walletRouter } from "./routes/wallet.js";
 
 const app = express()
 const PORT = 8000
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/api/users', userRouter);
 app.use('/api/stations', stationRouter);
 app.use('/api/trains', trainRouter);
+app.use('/api/wallets', walletRouter);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
